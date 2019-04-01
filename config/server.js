3537f3260dd6e1312fc7a7e62d8c5fd6e99f134d
Lfseibel <lfseibel@live.com>
2019-04-01 16:40:45 -0300

    Versão inicial

---
+++ config/server.js
@@ -0,0 +1,36 @@
+/* importar o modulo do framework express*/
+var express = require('express');
+
+/* importar o modulo do consign*/
+var consign = require('consign');
+
+/* importar o modulo do body-parser*/
+var bodyParser = require('body-parser');
+
+/* importar o modulo do express-validator*/
+var expressValidator = require('express-validator');
+
+/* iniciar o objeto do express*/
+var app = express();
+
+/* setar as variaveis 'view engine' e 'viewa' do express*/
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+/* configurar o middleware express.static*/
+app.use(express.static('./app/public'));
+
+/* configurar o middleware body-parser*/
+app.use(bodyParser.urlencoded({extended: true}));
+
+/* configurar o middleware express-validator*/
+app.use(expressValidator());
+
+/* configurar o consign, para fazer o autoload das rotas, models e controllers para o app*/
+consign()
+	.include('app/routes')
+	.then('app/models')
+	.then('app/controllers')
+	.into(app);
+/* exportar o objeto app*/
+module.exports = app;
